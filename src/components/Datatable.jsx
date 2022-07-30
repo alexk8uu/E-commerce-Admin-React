98c593cfa7d8dbc2e685a6480e38fa2b41d9dd35
alexk8uu <fredyalejandrocarrizo@gmail.com>
2022-07-30 01:50:51 -0300

    update

--- src/components/Datatable.jsx
+++ src/components/Datatable.jsx
@@ -1,66 +1,108 @@
-import styled from 'styled-components';
-
-import { DataGrid } from '@mui/x-data-grid';
-import { userColumns, userRows } from '../datatablesource';
-
+import styled from "styled-components";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import { DataGrid } from "@mui/x-data-grid";
+import { userColumns, userRows } from "../datatablesource";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
-    height: 550px;
-    padding: 20px;
+  height: 550px;
+  padding: 20px;
 `;
-
 
 const ContainerActions = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 15px;
+  display: flex;
+  align-items: center;
+  gap: 15px;
 `;
 
-
 const ViewButtom = styled.div`
-    padding: 2px 5px;
-    border-radius: 5px;
-    color: darkblue;
-    border: 1px dotted rgba(0,0,139,0.5);
-    cursor: pointer;
+  padding: 2px 5px;
+  border-radius: 5px;
+  color: darkblue;
+  border: 1px dotted rgba(0, 0, 139, 0.5);
+  cursor: pointer;
 `;
 const DeletedButtom = styled.div`
   padding: 2px 5px;
-    border-radius: 5px;
-    color: crimson;
-    border: 1px dotted rgba(220,20,60,0.6);
-    cursor: pointer;
+  border-radius: 5px;
+  color: crimson;
+  border: 1px dotted rgba(220, 20, 60, 0.6);
+  cursor: pointer;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  padding: 5px;
+  color: green;
+  font-size: 14px;
+  border: 1px solid;
+  font-weight: 500;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
 `;
 
 const actionColum = [
-    {
-        field: "action",
-        headerName: "Action",
-        width: 200,
-        renderCell: () => {
-            return (
-                <ContainerActions>
-                    <ViewButtom>View</ViewButtom>
-                    <DeletedButtom>Delete</DeletedButtom>
-                </ContainerActions>
-            )
-        }
-    }
-]
+  {
+    field: "action",
+    headerName: "Action",
+    width: 200,
+    renderCell: () => {
+      return (
+        <ContainerActions>
+          <StyledLink to="/users/test">
+            <ViewButtom>View</ViewButtom>
+          </StyledLink>
+          <DeletedButtom>Delete</DeletedButtom>
+        </ContainerActions>
+      );
+    },
+  },
+];
 
+const DatatableTitle = styled.div`
+  width: 100%;
+  font-size: 24px;
+  color: gray;
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Text = styled.h1`
+  font-size: 12px;
+  margin-right: 5px;
+`;
 
 const Datatable = () => {
-    return (
-        <Container>
-            <DataGrid
-                rows={userRows}
-                columns={userColumns.concat(actionColum)}
-                pageSize={9}
-                rowsPerPageOptions={[9]}
-                checkboxSelection
-            />
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <DatatableTitle>
+        Añadir Nuevo Usuario
+        <StyledLink to="/users/new">
+          <Text>Añadir</Text>
+          <PersonAddIcon />
+        </StyledLink>
+      </DatatableTitle>
+      <DataGrid
+        rows={userRows}
+        columns={userColumns.concat(actionColum)}
+        pageSize={9}
+        rowsPerPageOptions={[9]}
+        checkboxSelection
+      />
+    </Container>
+  );
+};
 
-export default Datatable
+export default Datatable;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,177 +1,190 @@
-import styled from 'styled-components';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
-import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
-import InventoryOutlinedIcon from '@mui/icons-material/InventoryOutlined';
-import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
-import QueryStatsOutlinedIcon from '@mui/icons-material/QueryStatsOutlined';
-import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
-import SettingsSystemDaydreamOutlinedIcon from '@mui/icons-material/SettingsSystemDaydreamOutlined';
-import ContentCopyOutlinedIcon from '@mui/icons-material/ContentCopyOutlined';
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
-
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import {
+  Dashboard,
+  ShoppingBagOutlined,
+  InventoryOutlined,
+  LocalShippingOutlined,
+  QueryStatsOutlined,
+  NotificationsNoneOutlined,
+  SettingsSystemDaydreamOutlined,
+  ContentCopyOutlined,
+  PeopleAltOutlined,
+  AccountCircleOutlined,
+  LogoutOutlined,
+  SettingsOutlined,
+} from "@mui/icons-material";
 
 const Container = styled.div`
-    flex: 1;
-    border-right: 0.5px solid rgba(230,227,227); 
-    min-height: 100vh;
-    background-color: white;
+  flex: 1;
+  border-right: 0.5px solid rgba(230, 227, 227);
+  min-height: 100vh;
+  background-color: white;
 `;
 
 const Top = styled.div`
-    height: 50px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 const Center = styled.div`
-    padding-left: 10px;
+  padding-left: 10px;
 `;
 const Bottom = styled.div`
-    display: flex;
-    align-items: center;
-    margin: 10px;
+  display: flex;
+  align-items: center;
+  margin: 10px;
 `;
 
 const ColorOption = styled.div`
-    width: 20px;
-    height: 20px;
-    border-radius: 5px;
-    border: 1px solid #745174;
-    cursor: pointer;
-    margin : 5px;
+  width: 20px;
+  height: 20px;
+  border-radius: 5px;
+  border: 1px solid #745174;
+  cursor: pointer;
+  margin: 5px;
 
-    &:nth-child(1){
-        background-color: white;
-    }
-    &:nth-child(2){
-        background-color: #333;
-    }
-
+  &:nth-child(1) {
+    background-color: white;
+  }
+  &:nth-child(2) {
+    background-color: #333;
+  }
 `;
 
-
 const Logo = styled.span`
-    font-size: 20px;
-    font-weight: bold;
-    color: violet;
+  font-size: 20px;
+  font-weight: bold;
+  color: violet;
 `;
 
 const ContainerList = styled.ul`
-    list-style: none;
-    margin: 0;
-    padding: 0;
-
+  list-style: none;
+  margin: 0;
+  padding: 0;
 `;
 const List = styled.li`
-    display: flex;
-    align-items: center;
-    padding: 5px;
-    cursor: pointer;
-    
+  display: flex;
+  align-items: center;
+  padding: 5px;
+  cursor: pointer;
 
-
-    &:hover {
-        background-color: #f4c7f4;
-    }
-
+  &:hover {
+    background-color: #f4c7f4;
+  }
 `;
 const ListText = styled.span`
-    font-size: 13px;
-    font-weight: 600;
-    color: #888;
-    margin-left: 10px;
+  font-size: 13px;
+  font-weight: 600;
+  color: #888;
+  margin-left: 10px;
 `;
 
 const ListTitle = styled.p`
-    font-size: 12px;
-    font-weight: bold;
-    color : #999;
-    margin-top: 15px;
-    margin-bottom: 5px;
+  font-size: 12px;
+  font-weight: bold;
+  color: #999;
+  margin-top: 15px;
+  margin-bottom: 5px;
 `;
-
 
 const Line = styled.hr`
-    height: 0;
-    border: 0.5px solid rgba(230,227,227);  
+  height: 0;
+  border: 0.5px solid rgba(230, 227, 227);
 `;
 
+const StyledLink = styled(Link)`
+  text-decoration: none;
 
-
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+`;
 
 const Sidebar = () => {
-    return (
-        <Container>
-            <Top>
-                <Logo>Magdalena</Logo>
-            </Top>
-            <Line />
-            <Center>
-                <ContainerList>
-                    <ListTitle>Principal</ListTitle>
-                    <List>
-                        <DashboardIcon color='secondary' fontSize='Small' />
-                        <ListText>Dashboard</ListText>
-                    </List>
-                    <ListTitle>Shoop</ListTitle>
-                    <List>
-                        <PeopleAltOutlinedIcon color='secondary' fontSize='Small'/>
-                        <ListText>Usuarios</ListText>
-                    </List>
-                    <List>
-                        <ShoppingBagOutlinedIcon color='secondary' fontSize='Small' />
-                        <ListText>Productos</ListText>
-                    </List>
-                    <List>
-                        <InventoryOutlinedIcon color='secondary' fontSize='Small' />
-                        <ListText>Ordenes</ListText>
-                    </List>
-                    <List>
-                        <LocalShippingOutlinedIcon color='secondary' fontSize='Small' />
-                        <ListText>Deliverys</ListText>
-                    </List>
-                    <ListTitle>Links Utiles</ListTitle>
-                    <List>
-                        <QueryStatsOutlinedIcon color='secondary' fontSize='Small' />
-                        <ListText>Stats</ListText>
-                    </List>
-                    <List>
-                        <NotificationsNoneOutlinedIcon  color='secondary' fontSize='Small' />
-                        <ListText>Notificaciones</ListText>
-                    </List>
-                    <ListTitle>Sistema</ListTitle>
-                    <List>
-                        <SettingsSystemDaydreamOutlinedIcon color='secondary' fontSize='Small' />
-                        <ListText>System Health</ListText>
-                    </List>
-                    <List>
-                        <ContentCopyOutlinedIcon color='secondary' fontSize='Small' />
-                        <ListText>Registros</ListText>
-                    </List>
-                    <List>
-                        <SettingsOutlinedIcon color='secondary' fontSize='Small' />
-                        <ListText>Configuracion</ListText>
-                    </List>
-                    <ListTitle>Usuario</ListTitle>
-                    <List>
-                        <AccountCircleOutlinedIcon color='secondary' fontSize='Small'  />
-                        <ListText>Perfil</ListText>
-                    </List>
-                    <List>
-                        <LogoutOutlinedIcon color='secondary' fontSize='Small' />
-                        <ListText>Salir</ListText>
-                    </List>
-                </ContainerList>
-            </Center>
-            <Bottom>
-                <ColorOption/>
-                <ColorOption/>
-            </Bottom>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Top>
+        <StyledLink to="/">
+          <Logo>Magdalena</Logo>
+        </StyledLink>
+      </Top>
+      <Line />
+      <Center>
+        <ContainerList>
+          <ListTitle>Principal</ListTitle>
+          <List>
+            <Dashboard color="secondary" fontSize="Small" />
+            <ListText>Dashboard</ListText>
+          </List>
+          <ListTitle>Shoop</ListTitle>
+          <StyledLink to="/users">
+            <List>
+              <PeopleAltOutlined color="secondary" fontSize="Small" />
+              <ListText>Usuarios</ListText>
+            </List>
+          </StyledLink>
+          <StyledLink to='/products'>
+            <List>
+              <ShoppingBagOutlined color="secondary" fontSize="Small" />
+              <ListText>Productos</ListText>
+            </List>
+          </StyledLink>
+          <List>
+            <InventoryOutlined color="secondary" fontSize="Small" />
+            <ListText>Ordenes</ListText>
+          </List>
+          <List>
+            <LocalShippingOutlined color="secondary" fontSize="Small" />
+            <ListText>Deliverys</ListText>
+          </List>
+          <ListTitle>Links Utiles</ListTitle>
+          <List>
+            <QueryStatsOutlined color="secondary" fontSize="Small" />
+            <ListText>Stats</ListText>
+          </List>
+          <List>
+            <NotificationsNoneOutlined color="secondary" fontSize="Small" />
+            <ListText>Notificaciones</ListText>
+          </List>
+          <ListTitle>Sistema</ListTitle>
+          <List>
+            <SettingsSystemDaydreamOutlined
+              color="secondary"
+              fontSize="Small"
+            />
+            <ListText>System Health</ListText>
+          </List>
+          <List>
+            <ContentCopyOutlined color="secondary" fontSize="Small" />
+            <ListText>Registros</ListText>
+          </List>
+          <List>
+            <SettingsOutlined color="secondary" fontSize="Small" />
+            <ListText>Configuracion</ListText>
+          </List>
+          <ListTitle>Usuario</ListTitle>
+          <List>
+            <AccountCircleOutlined color="secondary" fontSize="Small" />
+            <ListText>Perfil</ListText>
+          </List>
+          <List>
+            <LogoutOutlined color="secondary" fontSize="Small" />
+            <ListText>Salir</ListText>
+          </List>
+        </ContainerList>
+      </Center>
+      <Bottom>
+        <ColorOption />
+        <ColorOption />
+      </Bottom>
+    </Container>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
